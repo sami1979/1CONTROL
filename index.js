65fa6d4ebc663ca0abfcb84d1e469baa4f574ae1
sami1979 <nadim.gs.khoury@gmail.com>
2024-06-09 10:33:49 +0200

    soak label with soak mode

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 'use strict';
 
-function updateDisplay (rangeElement, valueElement, switchElement) {
+const SOAK_LABEL = 'soak-label';
+
+function updateDisplay (nameElement, rangeElement, valueElement, switchElement) {
   let value = rangeElement.value;
   if (switchElement.checked) {
     value = parseInt(value / 1.27).toFixed(0);
     value = String(value).concat(' %');
   }
   valueElement.textContent = value;
+
+  if (nameElement == 'Soak') {
+    const soakLabel = document.getElementById(SOAK_LABEL);
+    if (rangeElement.value < 65) {
+      soakLabel.innerText = 'SOAK Home';
+    } else if (rangeElement.value > 64) {
+      soakLabel.innerText = 'SOAK Stage';
+    }
+  }
 }
 
-function setControlDial (rangeElement, valueElement, switchElement, webMidiAPISend) {
-  updateDisplay(rangeElement, valueElement, switchElement);
+function setControlDial (nameElement, rangeElement, valueElement, switchElement, webMidiAPISend) {
+  updateDisplay(nameElement, rangeElement, valueElement, switchElement);
   rangeElement.addEventListener('input', function () {
-    updateDisplay(rangeElement, valueElement, switchElement);
+    updateDisplay(nameElement, rangeElement, valueElement, switchElement);
     webMidiAPISend(rangeElement.value);
   });
 }
@@ -42,6 +53,7 @@ const switchElement = document.getElementById('switch-percent-nominal');
 switchElement.addEventListener('change', function () {
   controlDials.forEach(function (controlDial) {
     updateDisplay(
+      controlDial.name,
       controlDial.rangeElement,
       controlDial.valueElement,
       switchElement);
@@ -50,6 +62,7 @@ switchElement.addEventListener('change', function () {
 
 controlDials.forEach(function (controlDial) {
   setControlDial(
+    controlDial.name,
     controlDial.rangeElement,
     controlDial.valueElement,
     switchElement, 
